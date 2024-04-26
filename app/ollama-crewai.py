178b618966e89d8e8e8ed8d49f1cf3390685f4cd
Sean Zheng <blackhorseya@gmail.com>
2024-04-27 07:35:21 +0800

    feat: implement chatbot functionality using OpenAI API
    - Add a new python file `ollama-crewai.py` containing code for a chatbot using OpenAI
    - Initialize OpenAI API key in the file
    - Define an Agent, Task, and Crew objects for the chatbot functionality
    - Kick off the chatbot functionality in the main block of the file
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ app/ollama-crewai.py
@@ -0,0 +1,35 @@
+import os
+
+from crewai import Agent, Task, Crew
+from langchain_openai import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = "NA"
+
+llm = ChatOpenAI(
+    model="llama3",
+    base_url="http://192.168.1.108:11434/v1"
+)
+
+general_agent = Agent(role="Math Professor",
+                      goal="""Provide the solution to the students that are asking mathematical questions and give 
+                      them the answer.""",
+                      backstory="""You are an excellent math professor that likes to solve math questions in a way 
+                      that everyone can understand your solution""",
+                      allow_delegation=False,
+                      verbose=True,
+                      llm=llm
+                      )
+task = Task(
+    description="""what is 3 + 5""",
+    expected_output="a text explaining the sum of the two num",
+    agent=general_agent
+)
+
+crew = Crew(
+    agents=[general_agent],
+    tasks=[task],
+)
+
+if __name__ == '__main__':
+    result = crew.kickoff()
+    print(result)
