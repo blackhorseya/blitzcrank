20e628d907894c5e41e2cab6657a93b47c494601
Sean Zheng <blackhorseya@gmail.com>
2024-05-10 00:50:33 +0800

    chore: update environment variables and refactor code structure
    - Add a new environment variable `OPENAI_MODEL_NAME=gpt-3.5-turbo` in the .env.example file
    - Remove the `from crewai import Task, Crew, Process` and `from crewai_tools import SerperDevTool` imports in main.py
    - Add `from crewai import Crew` import in main.py
    - Modify the tasks.py file to include a new method `__tip_section()`
    - Modify the fetch_news_task method in tasks.py to include a tip section and return a Task object
    - Modify the organize_data_task method in tasks.py to include a tip section and return a Task object
    - Modify the generate_markdown_task method in tasks.py to include a tip section and return a Task object
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/daily-news/main.py
+++ app/daily-news/main.py
@@ -1,7 +1,4 @@
-import os
-
-from crewai import Task, Crew, Process
-from crewai_tools import SerperDevTool
+from crewai import Crew
 from dotenv import load_dotenv
 
 from agents import Agents
@@ -9,53 +6,18 @@ from tasks import Tasks
 
 load_dotenv()
 
-search_tool = SerperDevTool()
-
-os.environ["OPENAI_MODEL_NAME"] = "gpt-3.5-turbo"
-
 topic = 'software engineering'
 
 agents = Agents()
 tasks = Tasks()
 
 news_collector = agents.collector_agent(topic)
-
-# Task for the News Collector to fetch news articles
-fetch_news_task = Task(
-    description=(
-        "Fetch the latest technology news articles related to {topic} from various sources."
-        "Focus on the most recent news and summarize the key points in a single document."
-    ),
-    expected_output='A single document containing the latest technology news articles on {topic}.',
-    tools=[search_tool],
-    agent=news_collector,
-)
-
-# Creating the Content Organizer Agent with memory and verbose mode
 content_organizer = agents.organizer_agent()
-
-# Task for organizing data from raw news articles
-organize_data_task = Task(
-    description=(
-        "Organize the raw news data into a structured format."
-        "Focus on extracting the headline, date, author, and summary of each news article."
-        "Present the organized data in a clear and concise manner."
-    ),
-    expected_output='A structured dataset containing the headline, date, author, and summary of each news article.',
-    tools=[],
-    agent=content_organizer,
-)
-
-# Creating the Markdown Generator Agent
 markdown_generator = agents.generator_agent()
 
-# Task to generate Markdown
-generate_markdown_task = Task(
-    description='Convert organized data into Markdown format.',
-    agent=markdown_generator,
-    expected_output='Markdown file',
-    output_file='tech_news_report.md'
-)
+fetch_news_task = tasks.fetch_news_task(news_collector, topic)
+organize_data_task = tasks.organize_data_task(content_organizer)
+generate_markdown_task = tasks.generate_markdown_task(markdown_generator)
 
 # Initialize the Crew with the News Collector Agent
 crew = Crew(
@@ -63,11 +25,6 @@ crew = Crew(
     agents=[news_collector, content_organizer, markdown_generator],
     tasks=[fetch_news_task, organize_data_task, generate_markdown_task],
     verbose=True,
-    process=Process.sequential,
-    memory=True,
-    cache=True,
-    max_rpm=100,
-    share_crew=True
 )
 
 if __name__ == '__main__':
--- app/daily-news/tasks.py
+++ app/daily-news/tasks.py
@@ -1,12 +1,58 @@
-from crewai import Agent
+from textwrap import dedent
+
+from crewai import Agent, Task
+from crewai_tools import SerperDevTool
+
+search_tool = SerperDevTool()
 
 
 class Tasks:
-    def fetch_news_task(self, agent: Agent, topic: str):
-        pass
+    @staticmethod
+    def __tip_section():
+        return "If you do your BEST WORK, I'll give you a $10,000 commission!"
 
-    def organize_data_task(self, agent: Agent):
-        pass
+    def fetch_news_task(self, agent: Agent, topic: str) -> Task:
+        return Task(
+            description=dedent(
+                f"""
+                Fetch the latest technology news articles related to {topic} from various sources. 
+                Focus on the most recent news and summarize the key points in a single document.
+                
+                {self.__tip_section()}
+                """
+            ),
+            expected_output=f'A single document containing the latest technology news articles on {topic}.',
+            tools=[search_tool],
+            agent=agent,
+        )
 
-    def generate_markdown_task(self, agent: Agent):
-        pass
+    def organize_data_task(self, agent: Agent) -> Task:
+        return Task(
+            description=dedent(
+                f"""
+                Organize the raw news data into a structured format. 
+                Focus on extracting the headline, date, author, and summary of each news article. 
+                Present the organized data in a clear and concise manner.
+                
+                {self.__tip_section()}
+                """
+            ),
+            expected_output=dedent(
+                f"""A structured dataset containing the headline, date, author, and summary of each news article."""
+            ),
+            agent=agent,
+        )
+
+    def generate_markdown_task(self, agent: Agent) -> Task:
+        return Task(
+            description=dedent(
+                f"""
+                Convert organized data into Markdown format.
+                
+                {self.__tip_section()}
+                """
+            ),
+            expected_output='Markdown file',
+            output_file='tech_news_report.md',
+            agent=agent,
+        )
