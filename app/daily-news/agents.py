eecdde19a2d298bd43b664f55c1add2c33b93514
Sean Zheng <blackhorseya@gmail.com>
2024-05-10 00:29:08 +0800

    feat: refactor Ollama base URL initialization in agents module
    - Add `OLLAMA_BASE_URL` to the `.env.example` file
    - Import `os` and `dedent` in `agents.py`
    - Add `Ollama` initialization with `OLLAMA_BASE_URL` in `agents.py`
    - Update `collector_agent` method signature and implementation in `agents.py`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/daily-news/agents.py
+++ app/daily-news/agents.py
@@ -1,12 +1,29 @@
+import os
+from textwrap import dedent
+
 from crewai import Agent
+from crewai_tools import SerperDevTool
+from langchain.llms import Ollama
+
+search_tool = SerperDevTool()
 
 
 class Agents:
     def __init__(self):
-        pass
+        self.Ollama = Ollama(model="llama3", base_url=os.getenv("OLLAMA_BASE_URL"))
 
-    def collector_agent(self) -> Agent:
-        pass
+    def collector_agent(self, topic: str) -> Agent:
+        return Agent(
+            role='Technology News Harvester',
+            goal=f'Collect daily technology news from various sources on {topic}.',
+            backstory=dedent(
+                """With a keen eye for detail, you're always on the lookout for the latest news and trends in the 
+                tech industry. Your goal is to keep up with the latest developments and share them with the world."""
+            ),
+            tools=[search_tool],
+            allow_delegation=True,
+            llm=self.Ollama,
+        )
 
     def organizer_agent(self) -> Agent:
         pass
