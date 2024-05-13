191ad273a430a426e67376078e08a131fd416ccd
Sean Zheng <blackhorseya@gmail.com>
2024-05-13 13:53:47 +0800

    docs: enhance documentation across multiple functions
    - Add docstring to the `collect_news` function
    - Improve the documentation for the function parameters
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/daily-news/news_service.py
+++ app/daily-news/news_service.py
@@ -1,4 +1,9 @@
 def collect_news(topic: str) -> str:
+    """
+    Collects news on the given topic.
+    :param topic:
+    :return:
+    """
     print(f'Collecting news on the {topic}...')
 
     return f'News collected on the {topic}.'
